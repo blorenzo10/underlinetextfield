61a7e369fed9228c1edb380fc9f75ebdd6cd3dec
Bruno Lorenzo <b.lorenzo@houlak.com>
2020-06-27 16:27:19 -0300

    Initial Commit

---
+++ CustomTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTextField
+//
+//  Created by Bruno Lorenzo on 6/27/20.
+//  Copyright © 2020 blorenzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
